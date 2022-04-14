b8ecd8c022ebfa8316d6f3ad7d522ee90f835e11
andresripoll <andresripollcabrera@gmail.com>
2022-04-14 18:15:33 +0200

    Instalación de ncclient

--- Instalacion/instalacion.py
+++ Instalacion/instalacion.py
@@ -16,6 +16,10 @@ def instalar_clab():
 	os.chdir(os.getcwd() + "/clab-quickstart")
 	call("cp -a /etc/containerlab/lab-examples/srlceos01/* .", shell=True)
 
+#Instala ncclient
+def instalar_ncclient():
+	call("pip install ncclient", shell=True)
+
 #Instala docker
 def instalar_docker():
 	call("sudo apt install docker.io", shell=True)
@@ -24,14 +28,19 @@ def instalar_docker():
 def mostrar_ayuda():
 	ayuda_clab()
 	print("")
+	ayuda_ncclient()
+	print("")
 	ayuda_docker()
 	print("")
-	print("help <cmd>             ---> para mostrar esta pantalla de ayuda o solo la ayuda de un comando.")
+	print("help <cmd>               ---> para mostrar esta pantalla de ayuda o solo la ayuda de un comando.")
 	print("\tParámetros:")
 	print("\t\t<cmd> -> opcional: para mostrar la ayuda de un único comando.")
 
 def ayuda_clab():
 	print("containerlab             ---> para instalar containerlab.")
+
+def ayuda_ncclient():
+	print("ncclient                 ---> para instalar ncclient.")
 
 def ayuda_docker():
 	print("docker                   ---> para instalar docker.")
@@ -41,18 +50,23 @@ def ayuda_docker():
 #Comprobamos que nos pasan más de dos argumentos
 if len(sys.argv) < 2:
 	print("Comandos disponibles:")
+	print("")
 	mostrar_ayuda()
 	sys.exit()
 
 #Ordenes
 if sys.argv[1] == "containerlab":
 	instalar_clab()
+elif sys.argv[1] == "ncclient":
+	instalar_ncclient()
 elif sys.argv[1] == "docker":
 	instalar_docker()
 elif sys.argv[1] == "help":
 	if len(sys.argv) > 2:
 		if (sys.argv[2] == "containerlab"):
 			ayuda_clab()
+		elif (sys.argv[2] == "ncclient"):
+			ayuda_ncclient()
 		elif (sys.argv[2] == "docker"):
 			ayuda_docker()
 		else:
